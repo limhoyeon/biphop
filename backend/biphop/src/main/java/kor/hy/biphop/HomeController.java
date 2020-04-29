3817276e2580c3517e4252ad9ae624a60035a88e
hoyeon <hoyeon351@gmail.com>
2020-04-29 18:37:27 +0900

    no message

--- backend/biphop/src/main/java/kor/hy/biphop/HomeController.java
+++ backend/biphop/src/main/java/kor/hy/biphop/HomeController.java
@@ -24,13 +24,12 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info("requested-url is / - get");
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
-			
 		return formattedDate;
 	}
 	
---
+++ frontend/install/src/components/newEqaulRegions.vue
@@ -0,0 +1,49 @@
+<template>
+<div>
+<div class="input-group mb-3">
+  <div class="input-group-prepend" id="button-addon3">
+    <button class="btn btn-outline-secondary" type="button">Button</button>
+    <button class="btn btn-outline-secondary" type="button">Button</button>
+  </div>
+  <input type="text" class="form-control" placeholder="" aria-label="Example text with two button addons" aria-describedby="button-addon3">
+</div>
+
+    <div class="input-group move-time mt-3 mb-3 mr-auto ml-auto w-75">
+        <div class="input-group-prepend" id="button-addon3">
+            <button class="btn btn-outline-secondary" type="button">Button</button>
+            <button class="btn btn-outline-secondary" type="button">Button</button>
+        </div>
+        <input type="text" class="form-control" placeholder="" aria-label="Example text with two button addons" aria-describedby="button-addon3">
+    </div>
+    <div class="input-group move-time mt-3 mb-3 mr-auto ml-auto w-75">
+        <div class="input-group-prepend" id="button-addon3">
+            <button class="btn btn-outline-secondary" type="button">Button</button>
+            <button class="btn btn-outline-secondary" type="button">Button</button>
+        </div>
+        <input type="text" class="form-control" placeholder="" aria-label="Example text with two button addons" aria-describedby="button-addon3">
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            skipTime: 1,
+            startTime: 0,
+            endTime: 0
+        }
+    },
+    props: {
+        'wavesurfer': Object
+    },
+    methods: {
+        hello() {
+            console.log("clicked")
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
