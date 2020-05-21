e5220c9fafcd6999cc67d26154938824677739b3
hoyeon <hoyeon351@gmail.com>
2020-05-21 18:53:00 +0900

    no message

--- frontend/src/store/actions.js
+++ frontend/src/store/actions.js
@@ -1,4 +1,4 @@
-import { USER_ID, IS_AUTH, TOKEN, SETWAVESURFER, ADDPLAYLIST, SETNOWPLAY , CLEARPLAYLIST } from './mutation_type'
+import { USER_ID, IS_AUTH, TOKEN, SETWAVESURFER, ADDPLAYLIST, SETNOWPLAY , CLEARPLAYLIST, SETPLAYLIST } from './mutation_type'
 import api from '@/utils/api'
 
 let clearPlaylist = ({commit})=>{
@@ -23,19 +23,19 @@ let addPlaylist = ({ commit }, data) => {
 let setNowPlay = ({ commit }, data) => {
   commit(SETNOWPLAY, data)
 }
-let initPlaylist = store => {
+let initPlaylist = ({commit}) => {
 
   api.get("/api/playlist/getPlayList")
-    .then((result) =>
-      result.data.forEach((data, index) => {
-        addPlaylist(store, data);
-        if (result.data.length > 0) {
-          setNowPlay(store, 0)
-        }
-      }))
+    .then((result) =>{
+      commit(SETPLAYLIST,result.data);
+    })
     .catch((err)=>{
       console.log(err)
+      alert("음악을 담는 중 오류 발생!")
     })
+}
+let setPlaylist = ({commit} , data)=>{
+  commit(SETPLAYLIST,data)
 }
 export default {
   loginProcess(store, { user_id, token }) {
@@ -69,11 +69,14 @@ export default {
   addPlaylistProcess(store, beat) {
     api.post("/api/playlist/addPlaylist", { music_idx: beat.music_idx })
     .then(res =>{
-      console.log(res)
       if (res.data.result === 0) {
         addPlaylist(store, beat);
         setNowPlay(store, 0);
       }      
+    })
+    .catch(err=>{
+      console.log(err)
+      alert("음악을 담는 중 오류 발생!")
     })
   },
   setNowPlayProcess(store, index) {
